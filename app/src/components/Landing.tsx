e94e7b32ee2f6ceb5dbc7ffa3f99095d452b0ac7
Lee Suan <leesuan97@gmail.com>
2023-02-11 00:53:33 -0800

    created initial landing page

--- app/src/components/Landing.tsx
+++ app/src/components/Landing.tsx
@@ -1,10 +1,27 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import Loading from "./shared/Loading";
 
 function Landing() {
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+
   return (
-    <section>
-      <h1>Are you down to LinkUp?</h1>
-      <Link href="/login">Get Started</Link>
+    <section className="flex flex-col justify-center gap-12 px-8 text-center">
+      <h1 className="text-3xl font-semibold">Ready to LinkUp?</h1>
+      <p className="text-center text-xl">
+        Plan big meetings with ease! Manage your schedules and events all in one
+        place.
+      </p>
+      <Link
+        href={status === "authenticated" ? "/dashboard" : "/login"}
+        className="rounded-lg bg-neutral-500 p-2 hover:bg-neutral-300 hover:text-black"
+      >
+        Get Started
+      </Link>
     </section>
   );
 }
