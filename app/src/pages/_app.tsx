ef1f59147eb0cc2f76deecec48c89888826aeb07
Lee Suan <leesuan97@gmail.com>
2023-02-21 02:48:13 -0800

    added new fonts

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable react/prop-types */
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Inter, Montserrat } from "@next/font/google";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
@@ -15,16 +17,29 @@ import "../styles/globals.css";
 
 config.autoAddCss = false;
 
+const inter = Inter({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+const montserrat = Montserrat({
+  weight: ["500", "600"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
 const MyApp: AppType<{ session: Session | null }> = function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <div className={`${inter.variable} ${montserrat.variable}`}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </div>
     </SessionProvider>
   );
 };
--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,8 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const lineClamp = require("@tailwindcss/line-clamp");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)", ...fontFamily.sans],
+        montserrat: ["var(--font-montserrat)", ...fontFamily.sans],
+      },
+    },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [lineClamp],
 };
