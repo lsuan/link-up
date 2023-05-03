c129d0a146ac758d449b4d7dff8bbb71b2185a46
Steph T <wytseng@uci.edu>
2023-05-02 17:45:09 -0700

    add brand colors to tw.config + create Hyperlink component

---
+++ app/src/components/ui/Hyperlink.tsx
@@ -0,0 +1,71 @@
+import { cva, type VariantProps } from "cva";
+import Link from "next/link";
+import {
+  type AnchorHTMLAttributes,
+  // type ClassAttributes,
+} from "react";
+// import { FiRotateCw } from "react-icons/fi";
+import { twMerge } from "tailwind-merge";
+
+const HyperLinkStyles = cva(
+  "transition-all font-inter px-1 py-0.5 border-2 rounded border-transparent text-base font-semibold underline underline-offset-2",
+  {
+    variants: {
+      intent: {
+        default: "text-brand-600 hover:text-brand-800 active:text-brand-900 focus:text-brand-600 focus:border-brand-600",
+        disabled: "text-disabled-400",
+      },
+      fullWidth: {
+        true: "w-full",
+      },
+    },
+    defaultVariants: {
+      intent: "default",
+    },
+  }
+);
+
+type HyperLinkProps = {
+  isLoading?: boolean;
+} & AnchorHTMLAttributes<HTMLAnchorElement> &
+  VariantProps<typeof HyperLinkStyles>;
+
+function HyperLink({
+  intent,
+  fullWidth,
+  children,
+  className,
+  ...rest
+}: HyperLinkProps) {
+  const isInternal = rest.href?.startsWith("/");
+  return (
+    <>
+      {isInternal && (
+        <Link
+          href={rest.href as string}
+          {...rest}
+        >
+          <span
+            className={twMerge(HyperLinkStyles({ intent, fullWidth, className }))}
+          >
+            {children}
+          </span>
+        </Link>
+      )}
+      {!isInternal && (
+        <a
+          href={rest.href as string}
+          {...rest}
+        >
+          <span
+            className={twMerge(HyperLinkStyles({ intent, fullWidth, className }))}
+          >
+            {children}
+          </span>
+        </a>
+      )}
+    </>
+  );
+}
+
+export default HyperLink;
